eda7eb8206332376be8382a4587f4ec54c07aff4
growwsocial <growwsocialshop@gmail.com>
2024-09-10 22:10:28 +0530

    Create combined_script.sh

---
+++ combined_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# First part of the script
+echo "Hello from the first part of the Bash script!"
+
+# Second part of the script
+echo "Hello from the second part of the Bash script!"
+combined_script.sh
